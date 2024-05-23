64d4ed1ab091e22874a28d07c14ce4b79dbcfc94
stix2you <stix2you@gmail.com>
2024-05-23 18:02:47 -0500

    documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+// This file defines the root components of the application
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,8 @@ import { Component } from '@angular/core';
    templateUrl: './app.component.html',
    styleUrls: ['./app.component.scss']
 })
+
+// Class that defines the component itself
 export class AppComponent {
    title = 'myFlix-Angular-client';
 }
--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -1,5 +1,4 @@
-// This file is where you create all the API calls for the client app
-
+// This file is where we create all the API calls for the client app
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError, of } from 'rxjs';
@@ -32,6 +31,7 @@ export class FetchApiDataService {
       );
    }
 
+   // Making the api call for the user profile endpoint, public so it can be accessed by the profile component
    public getUserProfile(username: string): Observable<any> {
       const token = localStorage.getItem('token');
       return this.http.get(`${apiUrl}users/${username}`, {
@@ -44,6 +44,7 @@ export class FetchApiDataService {
       );
    }
 
+   // Making the api call for the user profile update endpoint, public so it can be accessed by the profile component
    public updateUserProfile(username: string, userDetails: any): Observable<any> {
       const token = localStorage.getItem('token');
       return this.http.put(`${apiUrl}users/${username}`, userDetails, {
@@ -55,6 +56,7 @@ export class FetchApiDataService {
       );
    }
 
+   // Making the api call for the user account deletion endpoint, public so it can be accessed by the profile component
    public deleteUserAccount(username: string): Observable<any> {
       const token = localStorage.getItem('token');
       return this.http.delete(`${apiUrl}users/${username}`, {
@@ -66,6 +68,7 @@ export class FetchApiDataService {
       );
    }
 
+   // Making the api call for the movie list endpoint, public so it can be accessed by the movie-details component
    public getGenreInfo(genre: string): Observable<any> {
       const token = localStorage.getItem('token');
       return this.http.get(`${apiUrl}genres/${genre}`, {
@@ -78,6 +81,7 @@ export class FetchApiDataService {
       );
    }
 
+   // Making the api call for the movie list endpoint, public so it can be accessed by the movie-details component
    public getDirectorInfo(director: string): Observable<any> {
       const token = localStorage.getItem('token');
       return this.http.get(`${apiUrl}directors/${director}`, {
@@ -90,6 +94,7 @@ export class FetchApiDataService {
       );
    }
 
+   // Making the api call for the movie list endpoint
    getAllMovies(): Observable<any> {
       const token = localStorage.getItem('token');
       return this.http.get(apiUrl + 'movies', {
@@ -109,6 +114,7 @@ export class FetchApiDataService {
       return body || {};
    }
 
+   // This function will make a call to the database to retrieve a movie by title
    getOneMovie(title: string): Observable<any> {
       const token = localStorage.getItem('token');
       return this.http.get(`${apiUrl}movies/${title}`, {
@@ -121,6 +127,7 @@ export class FetchApiDataService {
       );
    }
 
+   // This function will make a call to the database to retrieve a director by name
    getDirector(name: String): Observable<any> {
       const token = localStorage.getItem('token');
       return this.http.get(apiUrl + `movies/director/${name}`, {
@@ -134,6 +141,7 @@ export class FetchApiDataService {
       );
    }
 
+   // This function will make a call to the database to retrieve a genre by name
    getGenre(name: String): Observable<any> {
       const token = localStorage.getItem('token');
       return this.http.get(apiUrl + `movies/genre/${name}`, {
@@ -147,6 +155,7 @@ export class FetchApiDataService {
       );
    }
 
+   // This function will make a call to the database to retrieve a user by username
    getUser(): Observable<any> {
       const token = localStorage.getItem('token');
       const username = localStorage.getItem('user');
@@ -161,6 +170,7 @@ export class FetchApiDataService {
       );
    }
 
+   // This function will make a call to the database to retrieve a user's favorite movies
    getFavoriteMovie(movie: any): Observable<any> {
       const token = localStorage.getItem('token');
       const username = localStorage.getItem('user');
@@ -175,6 +185,7 @@ export class FetchApiDataService {
       );
    }
 
+   // This function will make a call to the database to add a movie to a user's list of favorites
    public addFavoriteMovie(title: string): Observable<any> {
       const token = localStorage.getItem('token');
       const username = localStorage.getItem('username'); // Ensure correct retrieval of username
@@ -188,6 +199,7 @@ export class FetchApiDataService {
       );
    }
 
+   // This function will make a call to the database to remove a movie from a user's list of favorites
    public removeFavoriteMovie(title: string): Observable<any> {
       const token = localStorage.getItem('token');
       const username = localStorage.getItem('username');
@@ -203,6 +215,7 @@ export class FetchApiDataService {
       );
    }
 
+   // This function will make a call to the database to update a user's information
    editUser(userDetails: any): Observable<any> {
       const token = localStorage.getItem('token');
       const username = localStorage.getItem('user');
@@ -217,6 +230,7 @@ export class FetchApiDataService {
       );
    }
 
+   // This function will make a call to the database to delete a user's account
    deleteUser(): Observable<any> {
       const token = localStorage.getItem('token');
       const username = localStorage.getItem('user');
@@ -231,6 +245,7 @@ export class FetchApiDataService {
       );
    }
 
+   // This function will make a call to the database to delete a movie from a user's list of favorites
    deleteFavoriteMovie(title: any): Observable<any> {
       const token = localStorage.getItem('token');
       const username = localStorage.getItem('user');
@@ -245,11 +260,11 @@ export class FetchApiDataService {
       );
    }
 
+   // Error handling
    private handleError(error: HttpErrorResponse): Observable<any> {
       if (error.status === 200) {
          return of({});
       }
-
       if (error.error instanceof ErrorEvent) {
          console.error('An error occurred:', error.error.message);
       } else {
@@ -257,7 +272,6 @@ export class FetchApiDataService {
             `Backend returned code ${error.status}, ` +
             `body was: ${JSON.stringify(error.error)}`);
       }
-
       return throwError(
          'Operation failed; please try again later.');
    }
